1ba35acf55d1d4cda0b7633af3df7a7f45b6d315
jjl <jlyden1@alumni.nd.edu>
2022-12-04 13:15:39 -0500

    2022 finish day four part two

--- puzzles-2022/solution-04.py
+++ puzzles-2022/solution-04.py
@@ -4,14 +4,26 @@ from helpers import files
 
 def part_one():
     lines = files.get_contents_of_input_file('input-04.txt')
-    return get_subset_count_of_assignments(lines)
+    return get_count_of_assignment_relationship(lines, 'subset')
 
-def get_subset_count_of_assignments(lines):
+def part_two():
+    lines = files.get_contents_of_input_file('input-04.txt')
+    return get_count_of_assignment_relationship(lines, 'intersection')
+
+def get_count_of_assignment_relationship(lines, relationship):
+    match relationship:
+        case 'subset':
+            has_relationship_callback = _includes_subset
+        case 'intersection':
+            has_relationship_callback = _has_intersection
+        case _:
+            raise Exception('Invalid relationship: ' + relationship)
+
     count = 0
     for line in lines:
         assignments = line.replace('\n','').split(',')
         assignment_sets = [transform_to_set(assignment) for assignment in assignments]
-        count = count + 1 if includes_subset(assignment_sets) else count + 0
+        count = count + 1 if has_relationship_callback(assignment_sets) else count
     return count
 
 def transform_to_set(assignment):
@@ -19,7 +31,7 @@ def transform_to_set(assignment):
     # +1 b/c range upper bound is exclusive
     return set(range(boundaries[0], (boundaries[1]+1)))
 
-def includes_subset(list_of_two_sets):
+def _includes_subset(list_of_two_sets):
     set_one, set_two = list_of_two_sets
     if len(set_one) > len(set_two):
         larger_set = set_one
@@ -29,4 +41,9 @@ def includes_subset(list_of_two_sets):
         smaller_set = set_one
     return smaller_set.issubset(larger_set)
 
+def _has_intersection(list_of_two_sets):
+    set_one, set_two = list_of_two_sets
+    return len(set_one.intersection(set_two)) > 0
+
 print(part_one())
+print(part_two())
