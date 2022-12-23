f083bd74a5cca8a5a5e19fe2e4b2d6d2f43ff8cc
jjl <jlyden1@alumni.nd.edu>
2022-12-22 19:53:00 -0500

    partway through 2022 day 21 part two

--- puzzles-2022/solution-21.py
+++ puzzles-2022/solution-21.py
@@ -3,10 +3,13 @@
 from helpers import files
 
 def part_one():
-    lines = files.get_contents_of_input_file('input-21.txt')
+    lines = files.get_contents_of_input_file('input-mini.txt')
     simple_vars, operations = process_lines(lines)
     operations = substitute_simple_vars(simple_vars, operations)
-    return process_operations(operations)
+    process_result = process_operations(operations)
+    if isinstance(process_result, int):
+        return process_result
+    raise Exception('Expected int resolution of part_one process_operations')
 
 def process_lines(lines):
     simple_vars = {}
@@ -15,11 +18,10 @@ def process_lines(lines):
     for untrimmed_line in lines:
         line = untrimmed_line.replace('\n', '')
         line_parts = line.split(' ')
+        key = line_parts[0][0:-1]
         if len(line_parts) == 2:
-            [key, value] = line_parts
-            simple_vars[key[0:-1]] = int(value)
+            simple_vars[key] = int(line_parts[1])
         elif len(line_parts) == 4:
-            key = line_parts[0][0:-1]
             operations[key] = line_parts[1:]
         else:
             raise Exception('Unexpected line_parts: ' + str(line_parts))
@@ -47,10 +49,17 @@ def process_operations(operations):
 
     if len(simple_vars) > 0:
         updated_incomplete_ops = substitute_simple_vars(simple_vars, incomplete_ops)
-    if len(incomplete_ops) > 1:
-        return process_operations(updated_incomplete_ops)
-    if len(incomplete_ops) == 1:
-        return run_op(incomplete_ops['root'])
+        if len(updated_incomplete_ops) > 1:
+            return process_operations(updated_incomplete_ops)
+        if len(updated_incomplete_ops) == 1:
+            return run_op(updated_incomplete_ops['root'])
+#    elif 'root' in incomplete_ops:
+#        [val_one, _, val_two] = incomplete_ops['root']
+#        simple_vars[val_one] = val_two
+#        updated_incomplete_ops = substitute_simple_vars(simple_vars, incomplete_ops)
+#        return process_operations(updated_incomplete_ops)
+    else:
+        return incomplete_ops
 
 def run_op(operation):
     [val_one, op, val_two] = operation
@@ -66,4 +75,40 @@ def run_op(operation):
         case _:
             raise Exception('Invalid operation: ' + str(operation))
 
-print(part_one())
+def part_two():
+    lines = files.get_contents_of_input_file('input-mini.txt')
+    simple_vars, operations = process_lines_part_two(lines)
+    operations = substitute_simple_vars(simple_vars, operations)
+    process_result = process_operations(operations)
+    if isinstance(process_result, int):
+        raise Exception('Expected non-int resolution of part_two process_operations')
+    algebra_result = solve_algebraically(process_result)
+    return algebra_result
+
+def process_lines_part_two(lines):
+    simple_vars = {}
+    operations = {}
+
+    for untrimmed_line in lines:
+        line = untrimmed_line.replace('\n', '')
+        line_parts = line.split(' ')
+        key = line_parts[0][0:-1]
+        if key == 'humn': 
+            simple_vars[key] = key
+        elif key == 'root':
+            [val_one, _, val_two] = line_parts[1:]
+            operations[key] = [val_one, '=', val_two]
+        elif len(line_parts) == 2:
+            simple_vars[key] = int(line_parts[1])
+        elif len(line_parts) == 4:
+            operations[key] = line_parts[1:]
+        else:
+            raise Exception('Unexpected line_parts: ' + str(line_parts))
+    return simple_vars, operations
+
+def solve_algebraically(operations):
+    print(operations)
+    return
+
+#print(part_one())
+print(part_two())
